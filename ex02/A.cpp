efd118d7f659659f03645f9765da49996926015b
Quoc Tri Do <qdo@1-B-25.42heilbronn.de>
2024-06-20 16:40:08 +0200

    algorithm done

--- ex02/A.cpp
+++ ex02/A.cpp
@@ -6,7 +6,7 @@
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 12:17:41 by qdo               #+#    #+#             */
-/*   Updated: 2024/06/19 21:55:40 by qdo              ###   ########.fr       */
+/*   Updated: 2024/06/20 16:39:43 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,21 +46,4 @@ bool av_check(char **av)
 		if (!invalid_number(static_cast<std::string>(av[i])))
 			return (false);
 	return (true);
-}
-
-void generate(std::vector<int> & mVec, char **av)
-{
-	int i = 0;
-	while (av[++i])
-		mVec.push_back(std::atoi(av[i]));
-	return ;
-}
-
-void mVecPrint(std::vector<std::pair<int, int> > mVec)
-{
-	std::vector<std::pair<int, int> >::iterator ite = mVec.begin() - 1;
-	int	i = 0;
-	while (++ite != mVec.end())
-		std::cout << mVec.at(i++).first << " " << std::ends;
-	std::cout << std::endl;
 }
