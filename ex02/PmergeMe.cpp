c8a196c845f7a4f6eb9a1617efaeae1a95b26bad
TomTris <quoctrido.jb@gmail.com>
2024-06-20 16:59:56 +0200

    Done Algorithm

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,14 +1,71 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PMergeMe.cpp                                       :+:      :+:    :+:   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/19 11:45:47 by qdo               #+#    #+#             */
-/*   Updated: 2024/06/19 12:17:37 by qdo              ###   ########.fr       */
+/*   Created: 2024/06/19 12:17:41 by qdo               #+#    #+#             */
+/*   Updated: 2024/06/20 16:58:35 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
+int oprint(std::string str)
+{
+	std::cout << str << std::endl;
+	return (0);
+}
+int eprint(std::string str)
+{
+	std::cout << "Error: " << str << std::endl;
+	return (1);
+}
+
+bool invalid_number(std::string str)
+{
+	if (str.size() == 0)
+		return (eprint("Size = 0"), false);
+	std::string::iterator ite = str.begin();
+	if (*ite == '+' || *ite == '-')
+		ite++;
+	ite--;
+	while (++ite != str.end())
+	{
+		if (!std::isdigit(*ite))
+			return (eprint("Invalid character"), false);
+	}
+	return (true);
+}
+
+bool av_check(char **av)
+{
+	int	i = 0;
+	std::vector<int>  			check;
+	std::vector<int>::iterator  ite;
+	std::vector<int>::iterator  ite2;
+
+	while (av[++i])
+	{
+		if (!invalid_number(static_cast<std::string>(av[i])))
+			return (false);
+		check.push_back(std::atoi(av[i]));
+	}
+	ite = check.begin();
+	while (ite != check.end() - 1)
+	{
+		ite2 = ite + 1;
+		while (ite2 != check.end())
+		{
+			if (*ite2 == *ite)
+			{
+				eprint("Duplicate number");
+				return false;
+			}
+			ite2++;
+		}
+		ite++;
+	}
+	return (true);
+}
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 11:45:45 by qdo               #+#    #+#             */
-/*   Updated: 2024/06/19 12:18:06 by qdo              ###   ########.fr       */
+/*   Updated: 2024/06/20 16:49:03 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,35 @@
 #include <algorithm>
 #include <iostream>
 #include <sstream>
-
+#include <utility>
+#include <deque> 
 
 //A.cpp
 int				 oprint(std::string str);
 bool			 invalid_number(std::string str);
 bool			 av_check(char **av);
 void			 generate(std::vector<int> & mVec, char **av);
-void			 mVecPrint(std::vector<int> mVec);
+void			 mVecPrint(std::vector<std::pair<int, int> > mVec);
+
+
+void			 mVecPrint(std::vector<std::pair<int, int> > mVec);
+void			 generate(std::vector<int> & mVec, char **av);
+void			mmerge(std::vector<std::pair<int, int> > & mVec);
+void			set_pair_uper(std::vector<std::pair<int, int> > & mVec, std::vector<std::pair<int, int> > & uper);
+void			change_pair_lower(std::vector<std::pair<int, int> > &uper, std::vector<std::pair <int, int> > &lower);
+void			minsert(std::vector<std::pair<int, int> > &uper, std::vector<std::pair<int, int> > &lower);
+void			binary_sort(std::vector<std::pair<int, int> >&uper, std::pair<int, int> &lower,
+					unsigned int start, unsigned int end);
+unsigned int	jacob_gen(int check, std::vector<std::pair<int, int> > &lower);
+
+void			 dmVecPrint(std::deque<std::pair<int, int> > mVec);
+void			 dgenerate(std::deque<int> & mVec, char **av);
+void			dmmerge(std::deque<std::pair<int, int> > & mVec);
+void			dset_pair_uper(std::deque<std::pair<int, int> > & mVec, std::deque<std::pair<int, int> > & uper);
+void			dchange_pair_lower(std::deque<std::pair<int, int> > &uper, std::deque<std::pair <int, int> > &lower);
+void			dminsert(std::deque<std::pair<int, int> > &uper, std::deque<std::pair<int, int> > &lower);
+void			dbinary_sort(std::deque<std::pair<int, int> >&uper, std::pair<int, int> &lower,
+					unsigned int start, unsigned int end);
+unsigned int	djacob_gen(int check, std::deque<std::pair<int, int> > &lower);
+
 #endif
